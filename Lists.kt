f532abfc1da9ad3f1a57b680ba4430b04b807940
Haithamsaleh <92247964+Haithamsaleh@users.noreply.github.com>
2022-08-10 10:18:55 +0300

    Update Lists.kt

--- Lists.kt
+++ Lists.kt
@@ -1,4 +1,3 @@
-
 fun main() {
     
     // Read-only (immutable)
@@ -15,37 +14,80 @@ fun main() {
     
     
     // Add (Element)
-   	
+   	numbers.add("six ")
+    println(numbers)
+        println("===============================")
+    weekDays.add("sun")
+     println(weekDays)
+        println("===============================")
+
     // Add (Index, Element)
-   	
+     	numbers.add(2,"six ")
+   	    println(numbers)
+  println("===============================")
+
+weekDays.add(4,"Fri ")
+   	    println(weekDays)
+          println("===============================")
+
     // Add All (List)
+         	numbers.addAll(listOf("six ", "Seven"))
+   println(numbers)
+  println("===============================")
+      
+      
     // Add All (Index, List)
-    
+                 	numbers.addAll(2,weekDays)
+                       println(numbers)
+
+  println("===============================")
+
     // Clear 
-   	
+   	numbers.clear()
+                           println(numbers)
+
     // Remove
-    
+    weekDays.remove("Fri")
+  
+       println(weekDays)
+
     // Remove At 
-    
+    weekDays.removeAt(2)
+           println(weekDays)
+
     // Remove All
-   	
+   	weekDays.removeAll(numbers)
+               println(weekDays)
+
     
     // Read-only (immutable) List - Operations
      
      
     // The size of the list
+   val size = namesAll.size
     
+       println(size)
+
+
     // Contains
-   	
+       println("${names.contains("Fahdah")}")
+       println("${names.contains("haitham")}")
+
     // Contains All
-   	
+   	   	println("${names.containsAll(namesAll)}")
+
     // get
-   	
+   	   println("${namesAll.get(3)}")
+
     // indexOf
-    
+       println("${names.indexOf("Fahdah")}")
+       	   println("${namesAll.indexOf("Fahdah")}")
+
     // isEmpty
-    
+           println("${names.isEmpty()}")
+           println("${color.isEmpty()}")
+
     // Sublist
-    
-    
+    val supL = namesAll.subList(1,4)
+               println(supL)
 }
